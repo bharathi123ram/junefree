5f2e6b504c126f4c3983c2ef8d32e814813c0b9c
Mohammediqbal728 <mohammediqbal728@gmail.com>
2019-07-02 09:52:19 +0530

    Updetd code for testthree java prg

--- src/test/testthree.java
+++ src/test/testthree.java
@@ -3,15 +3,17 @@ package test;
 public class testthree {
 
 	public static void main(String[] args) {
-		int num=0,num1=1;
+		int num=0,num1=1,num=2;
 		int t;
 		System.out.println(num);
 		System.out.println(num1);
+		System.out.println(num2);
 		for (int i = 0; i <=5; i++) {
-			t=num+num1;
+			t=num+num1+num2;
 			System.out.println(t);
 			num=num1;
 			num1=t;
+			
 			
 			
 		}
