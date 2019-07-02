5eebecc0be8dfbdfb72ea56bb3682fea26c31b89
Saranyatmc1996 <saranyatmc1996@gmail.com>
2019-07-02 10:20:53 +0530

    New code added in testone from saranya

--- src/test/testone.java
+++ src/test/testone.java
@@ -16,9 +16,11 @@ public class testone {
 	    /* If number is divisible by 2 then it's an even number
 	     * else odd number*/
 	    if ( num % 2 == 0 )
+	    {
 	        System.out.println("Entered number is even");
-	     else
+	    }else
+	    {
 	        System.out.println("Entered number is odd");
 	  }
-	
+	  }
 }
